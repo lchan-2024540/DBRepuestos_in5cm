0bd528a66ed140d9213687b79862e2b39656fa74
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-02-13 18:16:51 -0600

    Cambios hechos

--- Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/repository/ProveedorRepository.java
+++ Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/repository/ProveedorRepository.java
@@ -4,6 +4,10 @@ import com.Luischan.RepuestosAutomotrices.entity.Proveedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProveedorRepository extends JpaRepository<Proveedor,Integer> {
+
+    Optional<Proveedor> findByNombreProveedor(String nombreProveedor);
 }
