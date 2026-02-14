c688e6ba601560d6ac5e347feda93ce4855b892d
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-02-13 18:16:31 -0600

    Cambios hechos

--- Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/repository/EmpleadoRepository.java
+++ Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/repository/EmpleadoRepository.java
@@ -4,9 +4,10 @@ import com.Luischan.RepuestosAutomotrices.entity.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface EmpleadoRepository extends JpaRepository<Empleado,Integer> {
+public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
 
-
-
+    Optional<Empleado> findByNombreEmpleadoAndApellidoEmpleado(String nombreEmpleado, String apellidoEmpleado);
 }
