b5e78b252b7598335a586141add8b955b5dbc499
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-02-13 21:29:15 -0600

    Archivo Subido

--- Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/service/VentaService.java
+++ Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/service/VentaService.java
@@ -1,4 +1,13 @@
 package com.Luischan.RepuestosAutomotrices.service;
 
+import com.Luischan.RepuestosAutomotrices.entity.Venta;
+
+import java.util.List;
+
 public interface VentaService {
+    List<Venta> getAllVentas();
+    Venta saveVenta(Venta venta);
+    Venta getVentaById(Integer id);
+    Venta updateVenta(Integer id, Venta venta);
+    void deleteVenta(Integer id);
 }
