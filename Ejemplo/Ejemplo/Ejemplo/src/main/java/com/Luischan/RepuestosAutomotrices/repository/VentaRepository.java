8ea08b2a7d3f990f977dd287cde1dc96b85107bc
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-02-13 21:28:32 -0600

    Archivo Subido

--- Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/repository/VentaRepository.java
+++ Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/repository/VentaRepository.java
@@ -1,4 +1,9 @@
 package com.Luischan.RepuestosAutomotrices.repository;
 
-public interface VentaRepository {
+import com.Luischan.RepuestosAutomotrices.entity.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentaRepository extends JpaRepository<Venta, Integer> {
 }
