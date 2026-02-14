d8d4bbe5a8c03f20243ae55920f04a0340acf076
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-02-13 21:48:34 -0600

    Archivo Editado

--- Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/repository/VentaRepository.java
+++ Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/repository/VentaRepository.java
@@ -4,6 +4,15 @@ import com.Luischan.RepuestosAutomotrices.entity.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
 public interface VentaRepository extends JpaRepository<Venta, Integer> {
+
+    Optional<Venta> findByFechaVentaAndIdEmpleadoAndIdRepuesto(
+            LocalDate fechaVenta,
+            Integer idEmpleado,
+            Integer idRepuesto
+    );
 }
