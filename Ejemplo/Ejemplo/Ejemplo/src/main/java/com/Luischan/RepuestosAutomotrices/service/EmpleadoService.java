8fc11fa70a010f7f48c7c81df3191417e5b6b890
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-02-13 18:17:16 -0600

    Cambios hechos

--- Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/service/EmpleadoService.java
+++ Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/service/EmpleadoService.java
@@ -1,15 +1,13 @@
 package com.Luischan.RepuestosAutomotrices.service;
 
 import com.Luischan.RepuestosAutomotrices.entity.Empleado;
-import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-@Service
 public interface EmpleadoService {
     List<Empleado> getAllEmpleados();
+    Empleado saveEmpleado(Empleado empleado);
     Empleado getEmpleadoById(Integer id);
-    Empleado saveEmpleado (Empleado empleado) throws RuntimeException;
     Empleado updateEmpleado(Integer id, Empleado empleado);
     void deleteEmpleado(Integer id);
-
 }
