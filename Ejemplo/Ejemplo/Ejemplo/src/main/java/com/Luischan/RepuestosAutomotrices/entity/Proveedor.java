35f5ccf0309d1fc685c1a7c366a796d9ee16f2a1
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-02-13 18:16:03 -0600

    Cambios hechos

--- Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/entity/Proveedor.java
+++ Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/entity/Proveedor.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 @Table(name ="Proveedores")
 public class Proveedor {
