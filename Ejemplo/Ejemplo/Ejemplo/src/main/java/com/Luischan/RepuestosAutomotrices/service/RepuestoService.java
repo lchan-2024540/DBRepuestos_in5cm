c79ca9d8912fb75bdec77694ab4457a2bd9dcbb0
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-02-13 20:41:45 -0600

    Archivo Subido

--- Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/service/RepuestoService.java
+++ Ejemplo/Ejemplo/Ejemplo/src/main/java/com/Luischan/RepuestosAutomotrices/service/RepuestoService.java
@@ -1,4 +1,13 @@
 package com.Luischan.RepuestosAutomotrices.service;
 
+import com.Luischan.RepuestosAutomotrices.entity.Repuesto;
+
+import java.util.List;
+
 public interface RepuestoService {
+    List<Repuesto> getAllRepuestos();
+    Repuesto saveRepuesto(Repuesto repuesto);
+    Repuesto getRepuestoById(Integer id);
+    Repuesto updateRepuesto(Integer id, Repuesto repuesto);
+    void deleteRepuesto(Integer id);
 }
